d31211defda34ee8152e673a8e4f16de307646c0
Lance Parsons <lparsons@princeton.edu>
2021-06-24 12:43:10 -0400

    Renamed project and application
    * Renamed the project to `config`
    
        * Since there is only ever one project but many apps within it,
        using a name like `config` is a [commonly used
        convention](https://learndjango.com/tutorials/django-best-practices-projects-vs-apps)
        that can make things less confusing
    
    * Renamed the application to neuronsimulator
    
       * Naming the application `neuronsimulator` may be a bit general, but
       this is likely a small project that may only have one application
    
       * Since an application is a python module, the name should follow
       [PEP8
       guidelines](https://www.python.org/dev/peps/pep-0008/#package-and-module-names)
       (it should be short, all-lowercase and not include numbers, dashes,
       periods, spaces, or special characters).
    
    The guidelines used in naming came from from [Django Best Practices:
    Projects vs
    Apps](https://learndjango.com/tutorials/django-best-practices-projects-vs-apps)
    from [LearnDjango](https://learndjango.com/)

--- FuncAtlas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FuncatlasConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "FuncAtlas"
---
+++ neuronsimulator/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NeuronSimulatorConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "neuronsimulator"
+    verbose_name = "Neuron Simulator"
