90a31a1bf01e6094e7d6883772ce51f94c0e07b1
fkang-pu <fkang@princeton.edu>
2021-08-11 13:09:53 -0400

    neuron model, data loading, and select/display input parameters

---
+++ neuronsimulator/management/commands/load_neurons.py
@@ -0,0 +1,25 @@
+from csv import DictReader
+
+from django.core.management import BaseCommand
+
+from neuronsimulator.models import Neuron
+
+
+class Command(BaseCommand):
+    # Show this when the user types help
+    help = "Loads data from a neuron list into the database"
+
+    def add_arguments(self, parser):
+        parser.add_argument("neuron_list_filename", type=str)
+
+    def handle(self, *args, **options):
+        print("Loading neuron data")
+        for row in DictReader(
+            open(options["neuron_list_filename"]),
+            dialect="excel-tab",
+        ):
+            # print(row['NAME'])
+            neuron, created = Neuron.objects.get_or_create(name=row["NAME"])
+            if created:
+                print(f"Created new record: Neuron:{neuron}")
+            neuron.save()
--- neuronsimulator/tests.py
+++ neuronsimulator/tests.py
@@ -1,5 +1,7 @@
+from django.core.management import call_command
 from django.test import TestCase
 from django.urls import reverse
+from neuronsimulator.models import Neuron
 
 
 class HomeViewTests(TestCase):
@@ -13,3 +15,25 @@ class HomeViewTests(TestCase):
         """
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
+
+
+class NeuronTests(TestCase):
+    def setUp(self):
+        Neuron.objects.create(name="ADAL")
+
+    def test_neuron_name(self):
+        """Neuron lookup by name"""
+        ADAL = Neuron.objects.get(name="ADAL")
+        self.assertEqual(ADAL.name, "ADAL")
+
+
+class DataLoadingTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        call_command(
+            "load_neurons", "neuronsimulator/example_data/worm_neuron_list.tsv"
+        )
+        cls.ALL_NEURONS_COUNT = 300
+
+    def test_neurons_loaded(self):
+        self.assertEqual(Neuron.objects.all().count(), self.ALL_NEURONS_COUNT)
