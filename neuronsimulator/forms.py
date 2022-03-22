50b20ff05f68bd35ebb41429811642a33c6ff5e4
fkang-pu <fkang@princeton.edu>
2022-03-22 15:41:23 -0400

    redo form, views, and templates

--- neuronsimulator/forms.py
+++ neuronsimulator/forms.py
@@ -1,110 +1,119 @@
 from django import forms
-from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from neuronsimulator.models import Neuron
 
 STIM_TYPE_CHOICES = (
     ("rectangular", "rectangular"),
     ("delta", "delta"),
-    ("sine", "sine"),
+    ("sinusoidal", "sinusoidal"),
     ("realistic", "realistic"),
 )
 
+STRAIN_CHOICES = (
+    ("wild type", "wild type"),
+    ("mutant1", "mutant1"),
+)
 
-class NeuronInputParamForm(forms.Form):
-    stim_neu_id = forms.CharField(max_length=10, widget=forms.HiddenInput())
-    resp_neu_ids = forms.CharField(
-        max_length=3000, required=False, widget=forms.HiddenInput()
+
+class ParamForm(forms.Form):
+    def _neuron_choices():
+        queryset = Neuron.objects.all()
+        return [(c, c) for c in queryset.values_list("name", flat=True)]
+
+    stim_type = forms.ChoiceField(
+        choices=STIM_TYPE_CHOICES,
+        label="Type of standard stimulus",
+        initial="rectangular",
+        widget=forms.Select,
+    )
+    stim_neu_id = forms.ChoiceField(
+        choices=_neuron_choices,
+        initial="ADAL",
+        label="Stimulated neuron",
+        widget =forms.Select(
+            attrs={
+                "class": "selectpicker",
+                "data-live-search": "true",
+                "data-width": "fit",
+            }
+        ),      
+    )
+    resp_neu_ids = forms.MultipleChoiceField(
+        choices=_neuron_choices,
+        label="Responding neurons",
+        required=False,
+        widget=forms.SelectMultiple(
+            attrs={
+                "class": "selectpicker",
+                "data-live-search": "true",
+                "data-width": "fit",
+                "data-actions-box": "true"
+            }
+        ),
+    )
+    top_n = forms.IntegerField(
+        initial=0,
+        label="Top N Responses",
+        required=False,
+        help_text="If not None, return top_n responses with the the largest absolute peak amplitude",
+        widget=forms.NumberInput(attrs={"min": 0}),
     )
     nt = forms.IntegerField(
         initial=1000,
-        label="Number of time points(nt)",
+        label="Number of time points",
         validators=[MinValueValidator(1)],
+        widget=forms.HiddenInput(attrs={'min': 1}),
     )
-    # use t_max to replace dt
-    t_max = forms.DecimalField(
-        initial=100,
-        label="Maximum time(t_max)",
+    duration = forms.FloatField(
+        initial=1.0,
+        label="Duration (s)",
         validators=[MinValueValidator(0)],
-        help_text="value requirement: t_max>dur",
+        help_text="required parameter when stim_type is rectangular; minimum value is 0",
+        widget=forms.NumberInput(attrs={"step": 1, "min": 0.0}),
     )
-    stim_type = forms.ChoiceField(
-        choices=STIM_TYPE_CHOICES,
-        widget=forms.Select,
-        label="Type of standard stimulus(stim_type)",
-        initial="rectangular",
+    frequency = forms.FloatField(
+        initial=0.1,
+        label="Frequency (Hz)",
+        validators=[MinValueValidator(0), MaxValueValidator(0.25)],
+        help_text="required parameter when stim_type is sinusoidal; values ranging from 0 to 0.25",
+        widget=forms.NumberInput(attrs={"step": 0.01, "max": 0.25, "min": 0.0}),
     )
-    dur = forms.DecimalField(
-        initial=2.0,
-        label="Duration of the stimulus(dur)",
+    phi0 = forms.FloatField(
+        initial=0.0,
+        label="Phase",
+        validators=[MinValueValidator(0), MaxValueValidator(6.28)],
+        help_text="required parameter when stim_type is sinusoidal; values ranging from 0 to 6.28",
+        widget=forms.NumberInput(attrs={"step": 0.01, "max": 6.28, "min": 0.0}),
+    )
+    tau1 = forms.FloatField(
+        initial=1.0,
+        label="Timescale 1 (s)",
+        validators=[MinValueValidator(0.5), MaxValueValidator(100)],
+        help_text="required parameter when stim_type is realistic; values ranging from 0.5 to 100",
+        widget=forms.NumberInput(attrs={"step": 0.1, "max": 100, "min": 0.5}),
+    )
+    tau2 = forms.FloatField(
+        initial=0.8,
+        label="Timescale 2 (s)",
+        validators=[MinValueValidator(0.5), MaxValueValidator(100)],
+        help_text="required parameter when stim_type is realistic; values ranging from 0.5 to 100",
+        widget=forms.NumberInput(attrs={"step": 0.1, "max": 100, "min": 0.5}),
+    )
+    t_max = forms.FloatField(
+        initial=100,
+        label="Maximum time (t_max)",
         validators=[MinValueValidator(0.1)],
+    )
+    strain_type = forms.ChoiceField(
+        choices=STRAIN_CHOICES,
+        widget=forms.Select,
+        label="Type of strain",
+        initial="wild type",
     )
 
     def clean(self):
         cleaned_data = super().clean()
-
-        stim_neu_id = cleaned_data.get("stim_neu_id")
-        resp_neu_ids = cleaned_data.get("resp_neu_ids")
-
-        # a neuron cannot be selected for both stim_neu_id and resp_neu_ids
-        if resp_neu_ids is not None:
-            resp_neu_arr = cleaned_data.get("resp_neu_ids").split(",")
-            if stim_neu_id in resp_neu_arr:
-                raise forms.ValidationError(
-                    "Response neuron(s) cannot include stimulated neuron, please make a different selection."
-                )
-
-        # t_max should be larger than dur
-        t_max = float(cleaned_data.get("t_max"))
-        dur = float(cleaned_data.get("dur"))
-        if t_max < dur:
-            raise forms.ValidationError("t_max must be larger than dur.")
-
-    def clean_stim_neu_id(self):
-        neurons = Neuron.objects.values_list("name", flat=True).all()
-        stim_neu_id = self.cleaned_data.get("stim_neu_id", None)
-        if stim_neu_id not in neurons:
-            raise forms.ValidationError("stimulated neuron is not a valid neuron")
-        return stim_neu_id
-
-    def clean_resp_neu_ids(self):
-        neurons = Neuron.objects.values_list("name", flat=True).all()
-        resp_neu_ids = self.cleaned_data.get("resp_neu_ids", None)
-
-        """
-        resp_neu_ids could be None based on wormfunconn source code
-        but require at least one resp neuron at this stage of test
-        will deal with this case later: resp_neu_ids=None and use a threshold value
-        """
-        if len(resp_neu_ids) == 0:
-            raise forms.ValidationError(
-                "You must select at least one response neuron (for testing)."
-            )
-        else:
-            resp_neu_arr = resp_neu_ids.split(",")
-            invalid_neurons = [i for i in resp_neu_arr if i not in neurons]
-            if len(invalid_neurons) > 0:
-                raise forms.ValidationError(
-                    f"Invalid response neuron(s) encountered: [{', '.join(invalid_neurons)}]"
-                )
-        return resp_neu_ids
-
-
-# keep ParamForm for now (as a reference for evaluating NeuronInputParamForm and form render)
-class ParamForm(forms.Form):
-    stim_neuron = forms.CharField(
-        max_length=10, widget=forms.TextInput, label="stim_neu_id"
-    )
-    resp_neurons = forms.CharField(
-        max_length=3000, widget=forms.Textarea(attrs={"rows": 3}), label="resp_neu_ids"
-    )
-    nt = forms.IntegerField(initial=1000, label="nt", help_text="Number of time points")
-    dt = forms.DecimalField(initial=0.1, label="dt", help_text="Time step")
-    stim_type = forms.CharField(
-        widget=forms.TextInput,
-        label="stim_type",
-        initial="rectangular",
-        help_text="Type of standard stimulus",
-    )
-    dur = forms.IntegerField(
-        initial=2, label="dur", help_text="Duration of the stimulus"
-    )
+        top_n = cleaned_data["top_n"]
+        # check top_n value
+        if top_n is not None and top_n < 0:
+            raise forms.ValidationError(f"Negative value is not allowed for Top_n.")
--- neuronsimulator/urls.py
+++ neuronsimulator/urls.py
@@ -3,10 +3,4 @@ from neuronsimulator import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("list_input_params", views.list_input_params, name="list_input_params"),
-    path(
-        "plot_neural_responses",
-        views.plot_neural_responses,
-        name="plot_neural_responses",
-    ),
 ]
